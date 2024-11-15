87f82684374e8d6cf82ca16c5a81a2f23c8fab19
sstitz <91822527+sstitz@users.noreply.github.com>
2024-11-15 09:43:55 +0100

    added code snippets (to be finished)

--- snippets/posix_condition_variables_interface.c
+++ snippets/posix_condition_variables_interface.c
@@ -1,16 +1,16 @@
 int pthread_cond_init(pthread_cond_t* condVar,          // ptr to condition variable
-                       const pthread_condattr_t *attr)  // ptr to pthread_condattr_t 
+                      const pthread_condattr_t *attr)   // ptr to pthread_condattr_t 
                                                         // structure, often 0 
                                                         // (default attributes)
 // returns 0 if condition variable is initiated successfully
 
 
-int pthread_cond_destroy(pthread_cond_t* condVar)    // ptr to condition variable
+int pthread_cond_destroy(pthread_cond_t* condVar)       // ptr to condition variable
 // returns 0 if condition variable is destroyed successfully
 
 
-int pthread_cond_wait(pthread_cond_t* condVar,     // ptr to condition variable
-                      pthread_mutex_t* mutex)      // ptr to pthread_mutex_t instance
+int pthread_cond_wait(pthread_cond_t* condVar,      // ptr to condition variable
+                      pthread_mutex_t* mutex)       // ptr to pthread_mutex_t instance
 // returns 0 if waiting (blocking) is successful
 
 
