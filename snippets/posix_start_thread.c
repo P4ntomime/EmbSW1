bd279cdcb8cd15982a103bc8158d5f9fd6ba89f9
P4ntomime <laurinheitzer@gmail.com>
2025-02-09 20:15:50 +0100

    some reformatting and typo fixes. To be merged at a later date

--- snippets/posix_start_thread.c
+++ snippets/posix_start_thread.c
@@ -1,8 +1,7 @@
-int pthread_create(pthread_t* thread,               // ptr to pthread_t instance
-                   const pthread_attr_t* attr,      // ptr to pthread_attr_t 
-                                                    // structure, often 0 
-                                                    // (default attributes)
-                   void* (*startRoutine) (void*),   // function ptr to thread routine
-                   void* arg);                      // single argument that may be
-                                                    // passed to startRoutine
+int pthread_create(
+  pthread_t* thread,            // ptr to pthread_t instance
+  const pthread_attr_t* attr,   // ptr to pthread_attr_t  structure, often 0 
+                                // (default attributes)
+  void* (*startRoutine) (void*),// function ptr to thread routine
+  void* arg);                   // single argument that may be passed to startRoutine
 // returns 0 if thread is started successfully
